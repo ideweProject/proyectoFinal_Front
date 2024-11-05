e978c6659360adac79afceef06d5380a50dd2a5c
Pablo Janavel <janavelgp@gmail.com>
2024-11-05 15:35:53 -0300

    cambio

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
-import Home from "./assets/components/Home";
+import Home from "./components/Home";
 
 function App() {
   return (
