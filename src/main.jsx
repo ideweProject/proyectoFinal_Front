84f0cac26435f4ab4f59f560fcb465853092d24f
Pablo Janavel <janavelgp@gmail.com>
2024-11-05 18:16:54 -0300

    delete css

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,5 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
 import Home from "./components/Home";
-
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
