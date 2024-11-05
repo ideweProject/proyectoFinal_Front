43791dbf0ebe4a5640d881870a63bdad12da61b0
bruno-lima135 <bruno.lima.taborda@gmail.com>
2024-11-05 18:08:47 -0300

    update all

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,15 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from "./components/Home";
+
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import Login from "./components/Login.jsx";
+import Signin from "./components/SignUp.jsx";
+import Products from "./components/Products.jsx";
+
 const router = createBrowserRouter([
   {
     path: "/",
