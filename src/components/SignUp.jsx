577392249b93cbba7c68b93f2558af07ce310a80
bruno-lima135 <bruno.lima.taborda@gmail.com>
2024-11-07 17:29:55 -0300

    login and signup updates

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -2,41 +2,54 @@ import React from "react";
 
 function SignUp() {
   return (
-    <div className="container  d-flex justify-content-center align-items-center vh-100 ">
-      <div className="mainContainer">
-        <h2 className="mb-4">Create your Vibra account</h2>
-        <form className="w-100" style={{ maxWidth: "400px" }}>
-          <div className="form-group mb-4">
-            <label>Email:</label>
-            <input type="email" className="form-control" placeholder="Email" />
-          </div>
-          <div className="form-group mb-4">
-            <label>Name:</label>
-            <input type="name" className="form-control" placeholder="Name" />
-          </div>
-          <div className="form-group mb-4">
-            <label>LastName:</label>
-            <input
-              type="password"
-              className="form-control"
-              placeholder="LastName"
-            />
-          </div>
-          <div className="form-group mb-4">
-            <label>Password:</label>
-            <input
-              type="password"
-              className="form-control"
-              placeholder="Password"
-            />
+    <>
+      <div className="container  login-container d-flex flex-column justify-content-center">
+        <div className="row g-0 h-75 shadow rounded">
+          <div className="col-md-6  ">
+            <div className=" login-image text-center h-100 w-100">
+              <img src="./images/img_pages/vibracologowhite.webp" alt="" />
+              <h1 className="image-title text-white h-100 d-md-flex d-none align-items-center justify-content-center">
+                Lo hacemos saludable
+              </h1>
+            </div>
           </div>
 
-          <button type="submit" className="btn text-light bg-dark mb-3  w-100">
-            Sign up
-          </button>
-        </form>
+          <div className="col-md-6">
+            <div className="d-flex flex-column ms-5 p-3 h-75 mt-4  ">
+              <div>
+                <h2 className="title">Crea tu cuenta Vibra </h2>
+                <form className="w-100 mt-4" style={{ maxWidth: "400px" }}>
+                  <div className="form-group mb-4">
+                    <label className="text">Email</label>
+
+                    <input type="email" className="form-control text" />
+                  </div>
+
+                  <div className="form-group mb-4">
+                    <label className="text">Nombre</label>
+                    <input type="name" className="form-control text" />
+                  </div>
+                  <div className="form-group mb-4">
+                    <label className="text">Apellido</label>
+                    <input type="password" className="form-control" />
+                  </div>
+                  <div className="form-group mb-4">
+                    <label className="text">Password</label>
+                    <input type="password" className="form-control" />
+                  </div>
+                  <button
+                    type="submit"
+                    className="title btn text-light bg-dark mb-3  w-100"
+                  >
+                    Crear cuenta
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
