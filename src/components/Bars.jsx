c5bf484751da3e88603757b52f0fedcd2df69352
Pablo Janavel <janavelgp@gmail.com>
2024-11-07 17:48:23 -0300

    bars

--- src/components/Bars.jsx
+++ src/components/Bars.jsx
@@ -2,16 +2,6 @@ import React, { useEffect, useState } from "react";
 import ProductCard from "./ProductCard";
 import axios from "axios";
 
-const productosData = [
-  {
-    id: 1,
-    nombre: "Celery juice",
-    precio: "280,00",
-    imagen:
-      "https://static.wixstatic.com/media/a9b61d_c49afa33446142178af6161206a9ee4c~mv2.jpg/v1/fill/w_226,h_226,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/a9b61d_c49afa33446142178af6161206a9ee4c~mv2.jpg",
-  },
-];
-
 function Bars() {
   useEffect(() => {
     const getProducts = async () => {
