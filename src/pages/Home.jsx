9cd7f5bffeb987ba004fe4316d214bfcd29bff2f
Pablo Janavel <janavelgp@gmail.com>
2024-11-12 18:31:46 -0300

    user logged - change to user profile navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,16 +4,8 @@ import CarouselComp from "../components/CarouselComp";
 import SectionFeatures from "../components/SectionFeatures";
 import CarouselBrands from "../components/CarouselBrands";
 import ShippingHeader from "../components/ShippingHeader";
-import { useSelector } from "react-redux";
 
 function Home() {
-  const user = useSelector((state) => state.login);
-  console.log(user.token);
-  // if (!user.token) {
-  //   useEffect(() => {
-  //     navigate("/");
-  //   }, []);
-
   return (
     <div>
       <ShippingHeader />
