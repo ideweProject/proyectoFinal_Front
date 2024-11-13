e8339dc6a5013032c2e7da0a4d68ef734cec9198
bruno-lima135 <bruno.lima.taborda@gmail.com>
2024-11-13 09:11:46 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import NavbarComp from "./components/NavbarComp";
 import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer";
-
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-
 import React from "react";
 
+import { useSelector } from "react-redux";
+
 function App() {
+  const token = useSelector();
+
   return (
     <>
+      {}
       <NavbarComp />
       <Outlet />
       <Footer />
