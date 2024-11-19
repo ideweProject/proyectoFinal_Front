7ed225abe3d6ab772a6a8318e8ace1ca4a219f2d
Pablo Janavel <janavelgp@gmail.com>
2024-11-19 11:29:33 -0300

    juices searchbar

--- src/components/CarrouselProducts.jsx
+++ src/components/CarrouselProducts.jsx
@@ -1,26 +1,11 @@
-import React, { useEffect } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { useDispatch, useSelector } from "react-redux";
-import axios from "axios";
-import { saveJuices } from "../redux/juicesSlice";
+import { useSelector } from "react-redux";
 import CarCard from "./CardForCarrousell";
 
 function CarouselProducts() {
-  const dispatch = useDispatch();
-  const juices = useSelector((state) => state.juices);
-
-  useEffect(() => {
-    const getProducts = async () => {
-      const response = await axios.get(
-        `${import.meta.env.VITE_API_URL}/products/juices`
-      );
-      dispatch(saveJuices({ juices: response.data }));
-    };
-
-    getProducts();
-  }, [dispatch, juices.length]);
+  const juices = useSelector((state) => state.juices.items);
 
   const settings = {
     dots: false,
