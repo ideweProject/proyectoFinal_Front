7adf70e5d94a95ff766c9a2bf40c46881591a40a
Pablo Janavel <janavelgp@gmail.com>
2024-11-22 14:30:16 -0300

    juices -banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,40 +1,35 @@
 import React from "react";
 
-
 function Cartel() {
-
-
-
   return (
     <div className="container row bannerContainer mt-5">
-      <div className="col-4 divisor align-items-center naturales">
+      <div className=" col-lg-4 col-md-6 bannerContainer2  divisor align-items-center naturales">
         <div>
           <div>
             <img src="" alt="" />
           </div>
           <div className="d-flex justify-content-center">
-            <div className="fs-3 d-flex">Jugos 100% naturales</div>
+            <div className="fs-3 nat100Title d-flex">Jugos 100% naturales</div>
           </div>
-          <div>
+          <div className="bannerText">
             Hacemos envíos dentro de Montevideo, Ciudad de la Costa y Maldonado.
             Además, también podes pasar a buscar el pedido por el local.
           </div>
         </div>
       </div>
-      <div className="col-4 g-0 juicesImgCol">
+      <div className=" col-4 d-none d-lg-block g-0 juicesImgCol">
         <div className="bannerCenterImg w-100 d-inline-block  ">
           <img
             className=" imgJuices"
             src="../../images/img_pages/vibraco-card04.jpg"
             alt=""
           />
-        
         </div>
       </div>
-      <div className="col-4 divisor align-items-center freeze">
+      <div className=" col-lg-4 col-md-6  d-none d-md-block divisor freeze">
         <div>
-          <div className="fs-3">Freeze</div>
-          <div className="">
+          <div className="fs-3 freezeTitle">Freeze</div>
+          <div className="bannerText2">
             Se entregan los jugos congelados para que duren más tiempo en el
             freezer y conserven todos sus nutrientes por más tiempo.
           </div>
