ddaff9e93bcbfde58086b6eff50a5a5fbb0b0d07
ideweProject <idewe2024@gmail.com>
2024-11-27 10:59:50 -0300

    z

--- src/components/CardOrder.jsx
+++ src/components/CardOrder.jsx
@@ -9,7 +9,7 @@ function CardOrder(orderData) {
         className="container d-flex justify-content-center mt-1"
       >
         <div className=" border p-4 rounded ">
-          <div className="d-flex border-bottom ">
+          <div className="d-flex border-bottom order-main-container ">
             <div className="text-center  ">
               <p className="text">Id</p>
               <span className="text">{orderData.orderData.id}</span>
